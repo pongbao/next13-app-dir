71a13ae576adb5ba9c0a1a0be18b75bb1cd6a0fa
Zedrick Torres <torres.zsj@gmail.com>
2023-08-12 17:03:58 +0800

    Add search course functionality

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,33 @@
 "use client";
+import { useEffect, useState } from "react";
 import Courses from "./components/Courses";
+import LoadingPage from "./loading";
+import CourseSearch from "./components/CourseSearch";
 
 export default function HomePage() {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch("/api/courses");
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
   return (
     <div>
       <h1>Welcome to Traversy Media</h1>
-      <Courses />
+      <CourseSearch getSearchResults={(results) => setCourses(results)} />
+      <Courses courses={courses} />
     </div>
   );
 }
